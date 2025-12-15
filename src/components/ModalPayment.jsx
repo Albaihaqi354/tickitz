1fac5598c6ec4fba5bd6f5890bd4b3bbeaf8435b
Muhammad_Albaihaqi_Mustofa <bianalbaihaqi190@gmail.com>
2025-12-15 10:32:25 +0700

    feat[Component] add Component For page

---
+++ src/components/ModalPayment.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+function ModalPayment() {
+  return (
+    <>
+      <style>{`
+                @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Mulish:wght@400;500;600;700&display=swap');
+                
+                * {
+                font-family: 'Mulish', sans-serif;
+                }
+            `}</style>
+      <main>
+        <div className="absolute flex w-screen h-screen inset-0 items-center justify-center after:absolute after:inset-0 after:z-10 after:bg-black/50">
+          <div className="bg-white w-130 h-130 flex flex-col rounded-lg z-20">
+            <div className="p-5">
+              <p className="text-2xl font-semibold text-center">Payment Info</p>
+              <div className="flex justify-between items-center mt-10">
+                <p className="text-[#8692A6]">No. Rekening Virtual</p>
+                <div className="flex gap-5 items-center">
+                  <p className="font-bold">12321328913829724</p>
+                  <button className="border border-solid border-[#1D4ED8] text-[#1D4ED8] p-2 pl-3 pr-3 rounded-lg">
+                    Copy
+                  </button>
+                </div>
+              </div>
+              <div className="flex justify-between items-center mt-10">
+                <p className="text-[#8692A6]">Total Payment</p>
+                <p className="text-[#1D4ED8] text-xl font-bold">$30</p>
+              </div>
+              <div className="mt-10">
+                <p className="text-[#8692A6]">Pay this payment bill before it is due, on June 23, 2023. If the bill has not been paid by the specified time, it will be forfeited</p>
+              </div>
+            </div>
+            
+            <button className="bg-[#1D4ED8] font-bold rounded-lg text-white p-4 mt-5 ml-5 mr-5">Check Payment</button>
+            <button className="font-bold rounded-lg text-[#1D4ED8] p-3 m-5">Pay Later</button>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default ModalPayment;
---
+++ src/components/OrderHistory.jsx
@@ -0,0 +1,113 @@
+import CineOneLogo from "../assets/CineOne21.svg";
+import EbvLogo from '../assets/ebv.svg';
+
+function OrderHistory() {
+  return (
+    <main>
+      <style>{`
+        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Mulish:wght@400;500;600;700&display=swap');
+
+        * {
+          font-family: 'Mulish', sans-serif;
+        }
+      `}</style>
+      <section className="w-full">
+        <section className="bg-white m-10 rounded-3xl gap-15 p-10 pt-15">
+          <div className="flex justify-between items-center">
+            <div>
+              <p className="text-xl text-[#AAAAAA]">
+                Tuesday, 07 July 2020 - 04:30pm
+              </p>
+              <p className="text-3xl font-bold mt-5">Spider-Man: Homecoming</p>
+            </div>
+            <div>
+              <img
+                src={CineOneLogo}
+                alt="CineOne Logo"
+                className="w-32 md:w-36 lg:w-40 xl:w-45 h-auto"
+              />
+            </div>
+          </div>
+
+          <div className="border-t-2 border-[#DEDEDE] w-full mt-8"></div>
+
+          <div className="flex justify-between mt-10">
+            <div className="flex gap-5">
+              <button className="text-xl font-semibold text-[#00BA88] bg-[#00BA8833] rounded-xl p-3 pr-10 pl-10">
+                Ticket in active
+              </button>
+              <button className="text-xl font-semibold text-[#E82C2C] bg-[#E82C2C33] rounded-xl p-3 pr-10 pl-10">
+                Not Paid
+              </button>
+            </div>
+            <select name="" id="" className="text-[#AAAAAA] text-xl w-45">
+              <option value="">Show Details</option>
+            </select>
+          </div>
+        </section>
+
+        <section className="bg-white m-10 rounded-3xl gap-15 p-10 pt-15">
+          <div className="flex justify-between items-center">
+            <div>
+              <p className="text-xl text-[#AAAAAA]">
+                Monday, 14 June 2020 - 02:00pm
+              </p>
+              <p className="text-3xl font-bold mt-5">Avengers: End Game</p>
+            </div>
+            <div>
+                <img src={EbvLogo} alt="EBV Logo" className="w-20 md:w-32 lg:w-35 h-auto" />
+            </div>
+          </div>
+
+          <div className="border-t-2 border-[#DEDEDE] w-full mt-8"></div>
+
+          <div className="flex justify-between mt-10">
+            <div className="flex gap-5">
+              <button className="text-xl font-semibold text-[#6E7191] bg-[#6E719133] rounded-xl p-3 pr-10 pl-10">
+                Ticket used
+              </button>
+              <button className="text-xl font-semibold text-[#1D4ED8] bg-[#1D4ED833] rounded-xl p-3 pr-10 pl-10">
+                Paid
+              </button>
+            </div>
+            <select name="" id="" className="text-[#AAAAAA] text-xl w-45">
+              <option value="">Show Details</option>
+            </select>
+          </div>
+        </section>
+        
+        <section className="bg-white m-10 rounded-3xl gap-15 p-10 pt-15">
+          <div className="flex justify-between items-center">
+            <div>
+              <p className="text-xl text-[#AAAAAA]">
+                Monday, 14 June 2020 - 02:00pm
+              </p>
+              <p className="text-3xl font-bold mt-5">Avengers: End Game</p>
+            </div>
+            <div>
+                <img src={EbvLogo} alt="EBV Logo" className="w-20 md:w-32 lg:w-35 h-auto" />
+            </div>
+          </div>
+
+          <div className="border-t-2 border-[#DEDEDE] w-full mt-8"></div>
+
+          <div className="flex justify-between mt-10">
+            <div className="flex gap-5">
+              <button className="text-xl font-semibold text-[#6E7191] bg-[#6E719133] rounded-xl p-3 pr-10 pl-10">
+                Ticket used
+              </button>
+              <button className="text-xl font-semibold text-[#1D4ED8] bg-[#1D4ED833] rounded-xl p-3 pr-10 pl-10">
+                Paid
+              </button>
+            </div>
+            <select name="" id="" className="text-[#AAAAAA] text-xl w-45">
+              <option value="">Show Details</option>
+            </select>
+          </div>
+        </section>
+      </section>
+    </main>
+  );
+}
+
+export default OrderHistory;
