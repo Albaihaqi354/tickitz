cefbfa5457095926ba845d0e491c9262c7280c62
Muhammad_Albaihaqi_Mustofa <bianalbaihaqi190@gmail.com>
2025-12-15 10:11:40 +0700

    feat[main.jsx] add feature main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,19 @@ import { createRoot } from 'react-dom/client'
 // import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
 import Router from './Router.jsx'
+// import UserProvider from './contexts/userManagement/UserProvider.jsx'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from './redux/store' // HILANGKAN
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-        <Router/>
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}> 
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </StrictMode>,
 )
