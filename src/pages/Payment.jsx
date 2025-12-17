d9e16c0498024afd1c7120953b2ef43cf39bde47
Muhammad_Albaihaqi_Mustofa <bianalbaihaqi190@gmail.com>
2025-12-17 19:36:29 +0700

    fix[payment.jsx] fix layout payment.jsx

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -16,34 +16,34 @@ function Payment() {
       <Header />
       <div className="min-h-screen bg-gray-100 py-10 px-4">
         <div className="max-w-2xl mx-auto">
-          <section className='pt-10'>
-          <div className="hidden md:flex justify-center items-center gap-5 text-center mb-10">
-            <div className="flex flex-col items-center">
-              <p className="w-12 h-12 flex items-center justify-center bg-green-700 text-white rounded-full">
-                1
-              </p>
-              <p className="text-sm mt-1">Dates And Time</p>
+          <section className="pt-10">
+            <div className="hidden md:flex justify-center items-center gap-5 text-center mb-10">
+              <div className="flex flex-col items-center">
+                <p className="w-12 h-12 flex items-center justify-center bg-green-700 text-white rounded-full">
+                  1
+                </p>
+                <p className="text-sm mt-1">Dates And Time</p>
+              </div>
+
+              <div className="border-t-2 border-dashed border-gray-400 w-16 mb-6"></div>
+
+              <div className="flex flex-col items-center">
+                <p className="w-12 h-12 flex items-center justify-center bg-gray-400 text-white rounded-full">
+                  2
+                </p>
+                <p className="text-sm mt-1">Seat</p>
+              </div>
+
+              <div className="border-t-2 border-dashed border-gray-400 w-16 mb-6"></div>
+
+              <div className="flex flex-col items-center">
+                <p className="w-12 h-12 flex items-center justify-center bg-gray-400 text-white rounded-full">
+                  3
+                </p>
+                <p className="text-sm mt-1">Payment</p>
+              </div>
             </div>
-            
-            <div className="border-t-2 border-dashed border-gray-400 w-16 mb-6"></div>
-            
-            <div className="flex flex-col items-center">
-              <p className="w-12 h-12 flex items-center justify-center bg-gray-400 text-white rounded-full">
-                2
-              </p>
-              <p className="text-sm mt-1">Seat</p>
-            </div>
-            
-            <div className="border-t-2 border-dashed border-gray-400 w-16 mb-6"></div>
-            
-            <div className="flex flex-col items-center">
-              <p className="w-12 h-12 flex items-center justify-center bg-gray-400 text-white rounded-full">
-                3
-              </p>
-              <p className="text-sm mt-1">Payment</p>
-            </div>
-          </div>
-        </section>
+          </section>
 
           <div className="bg-white rounded-lg shadow-sm p-8">
             <div className="mb-10">
@@ -84,7 +84,9 @@ function Payment() {
                   <label className="text-sm font-semibold text-gray-400 uppercase tracking-wide">
                     Number of Tickets
                   </label>
-                  <p className="text-base font-semibold text-gray-700 mt-1">3 pieces</p>
+                  <p className="text-base font-semibold text-gray-700 mt-1">
+                    3 pieces
+                  </p>
                 </div>
                 <div className="border-t-2 border-[#E6E6E6] w-full"></div>
 
@@ -152,40 +154,36 @@ function Payment() {
               <h2 className="text-2xl font-semibold mb-6">Payment Method</h2>
 
               <div className="grid grid-cols-4 gap-4">
-                <button className="border border-gray-200 rounded-lg p-4 hover:border-blue-500 transition-colors">
-                  <img
-                    src={GooglePay}
-                    alt="Google Pay"
-                    className="w-28 mx-auto"
-                  />
+                <button className="border border-gray-200 rounded-lg h-20 p-4 hover:border-blue-500 transition-colors">
+                  <img src={GooglePay} alt="Google Pay" className="mx-auto"/>
                 </button>
 
-                <button className="border border-gray-200 rounded-lg p-4 hover:border-blue-500 transition-colors">
-                  <img src={Visa} alt="Visa" className="w-28 mx-auto" />
+                <button className="border border-gray-200 rounded-lg h-20 p-4 hover:border-blue-500 transition-colors">
+                  <img src={Visa} alt="Visa" className="mx-auto" />
                 </button>
 
-                <button className="border border-gray-200 rounded-lg p-4 hover:border-blue-500 transition-colors">
-                  <img src={GoPay} alt="GoPay" className="w-28 mx-auto" />
+                <button className="border border-gray-200 rounded-lg h-20 p-4 hover:border-blue-500 transition-colors">
+                  <img src={GoPay} alt="GoPay" className="mx-auto" />
                 </button>
 
-                <button className="border border-gray-200 rounded-lg p-4 hover:border-blue-500 transition-colors">
-                  <img src={Paypal} alt="Paypal" className="w-20 mx-auto" />
+                <button className="border border-gray-200 rounded-lg p-4 h-20 hover:border-blue-500 transition-colors">
+                  <img src={Paypal} alt="Paypal" className="mx-auto" />
                 </button>
 
-                <button className="border border-gray-200 rounded-lg p-4 hover:border-blue-500 transition-colors">
-                  <img src={Dana} alt="Dana" className="w-28 mx-auto" />
+                <button className="border border-gray-200 rounded-lg p-4 h-20 hover:border-blue-500 transition-colors">
+                  <img src={Dana} alt="Dana" className="mx-auto" />
                 </button>
 
-                <button className="border border-gray-200 rounded-lg p-4 hover:border-blue-500 transition-colors">
-                  <img src={Bca} alt="BCA" className="w-28 mx-auto" />
+                <button className="border border-gray-200 rounded-lg p-4 h-20 hover:border-blue-500 transition-colors">
+                  <img src={Bca} alt="BCA" className="mx-auto" />
                 </button>
 
-                <button className="border border-gray-200 rounded-lg p-4 hover:border-blue-500 transition-colors">
-                  <img src={Bri} alt="BRI" className="w-28 mx-auto" />
+                <button className="border border-gray-200 h-20 rounded-lg p-4 hover:border-blue-500 transition-colors">
+                  <img src={Bri} alt="BRI" className="mx-auto" />
                 </button>
 
-                <button className="border border-gray-200 rounded-lg p-4 hover:border-blue-500 transition-colors">
-                  <img src={Ovo} alt="OVO" className="w-28 mx-auto" />
+                <button className="border border-gray-200 h-20 rounded-lg p-4 hover:border-blue-500 transition-colors">
+                  <img src={Ovo} alt="OVO" className="mx-auto" />
                 </button>
               </div>
             </div>
